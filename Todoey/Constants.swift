f9c74230a100bedd23c0a1aea775f3646140dab8
notechus <seba0713@gmail.com>
2020-12-14 23:50:29 +0100

    Init

---
+++ Todoey/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  K.swift
+//  Todoey
+//
+//  Created by Sebastian Paulus on 14/12/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+struct K {
+    
+    struct TodoList {
+        static let cellName = "ToDoItemCell"
+    }
+}
---
+++ Todoey/Controller/TodoListViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    var itemArray = ["Groceries", "Christmas Gifts"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.TodoList.cellName, for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType =  cell?.accessoryType == .checkmark ? .none : .checkmark
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new todo", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (alertAction) in
+            if let todo = textField.text {
+                if todo != "" {
+                    self.itemArray.append(textField.text!)
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Type new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
